3496e00706f45f71595ff9ce8a0401b44deb9183
Karique Vera GKN <kvera@globokas.com>
2023-07-07 11:27:35 -0500

    onegaiii

--- app.js
+++ app.js
@@ -6,15 +6,7 @@ const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-app.use(cors({
-    origin: 'https://cursocrehanadebug.vercel.app'
-  }));
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
-  next();
-})
+app.use(cors());
 app.use(express.static('public'))
 app.listen(process.env.PORT || 3000);
 
