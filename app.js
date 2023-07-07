8291806dbdce72665ed3262fe8d302925734e485
Karique Vera GKN <kvera@globokas.com>
2023-07-07 10:33:08 -0500

    "fix llamada a api en prod de vercel"

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const cors = require('cors');
+//const cors = require('cors');
 
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-app.use(cors());
+//app.use(cors());
 app.use(express.static('public'))
 app.listen(process.env.PORT || 3000);
 
